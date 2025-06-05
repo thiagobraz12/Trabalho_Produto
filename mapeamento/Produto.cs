ab883dc2d6aa52378f12fceb949f55fb16fa2eee
Thiago Braz <thiagoadsbraz@gmail.com>
2025-06-04 23:00:22 -0400

    trabalho

---
+++ mapeamento/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_Produto.mapeamento
+{
+    internal class Produto
+    {
+        public int Id { get; set; }
+        public string nomeproduto { get; set; }
+        public double preco { get; set; }
+        public int QuantidadeEstoque { get; set; }
+        public string categoria { get; set; }
+        public DateOnly validade { get; set; }
+        public string marca { get; set; }
+        public string unidade { get; set; }
+
+    }
+}
---
+++ ultilitarios/Conexao.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_Produto.ultilitarios
+{
+    internal class Conexao
+    {
+        public static MySqlConnection conexao;
+
+        public static MySqlConnection Conectar()
+        {
+            try
+            {
+                string strconexao = "server=localhost;port=3306;uid=root;pwd=123456;database=CadastroProdutos";
+                conexao = new MySqlConnection(strconexao);
+                conexao.Open();
+                //Console.WriteLine("Conectado!");
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("erro ao conectar" + ex.Message);
+            }
+            return conexao;
+        }
+        public static void FecharConexao()
+        {
+            conexao.Clone();
+        }
+    }
+}
+
